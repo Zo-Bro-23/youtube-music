05f7e2b1136061a490826a64ade205dd433a88ac
Zohan Subhash <zohan.subhash@gmail.com>
2023-03-01 16:36:00 +0530

    -m

---
+++ plugins/api/menu.js
@@ -0,0 +1,10 @@
+module.exports = () => [
+    {
+        label: "Api test",
+        type: "checkbox",
+        checked: false,
+        click: () => {
+            console.log('clicked')
+        }
+    }
+];
