6f39aa632d758e23e31b7f7aea9d966b2b7b2011
Zohan Subhash <zohan.subhash@gmail.com>
2023-03-29 12:14:40 +0530

    New features

--- plugins/m-api/back.js
+++ plugins/m-api/back.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 
 const { setOptions, getOptions } = require("../../config/plugins");
 const registerCallback = require("../../providers/song-info");
+const getSongControls = require("../../providers/song-controls");
+const { ipcMain } = require("electron");
 
 module.exports = (win) => {
     win.once("ready-to-show", () => {
@@ -14,12 +16,36 @@ module.exports = (win) => {
                 })
         }
 
+        let currentSongInfo
+
         registerCallback(songInfo => {
+            currentSongInfo = songInfo
             axios.post("https://youtube-music-api.zohan.tech/api/status", { key: getOptions("m-api").key, ...songInfo })
                 .catch(err => {
-                    console.log(songInfo)
+                    console.log(err.message, err.stack, songInfo)
                 })
         })
+
+        ipcMain.on('timeChanged', (_, t) => {
+            if (currentSongInfo) {
+                currentSongInfo.elapsedSeconds = t
+                axios.post("https://youtube-music-api.zohan.tech/api/status", { key: getOptions("m-api").key, ...currentSongInfo })
+                    .catch(err => {
+                        console.log(err.message, err.stack, currentSongInfo)
+                    })
+            }
+        })
+
+        pollControls();
+
+        async function pollControls() {
+            const response = await axios.get(`https://youtube-music-api.zohan.tech/api/controls?key=${getOptions("m-api").key}`)
+            const songControls = getSongControls(win);
+            response.data.controls.forEach(control => {
+                songControls[control]();
+            })
+            setTimeout(pollControls, 100);
+        }
     })
 }
 
